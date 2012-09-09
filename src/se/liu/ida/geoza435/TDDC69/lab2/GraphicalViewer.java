cd829adb3aa37b7a4198d2ea9f8eb1966fad7e95
Georgij Zacharov <gosha.za@gmail.com>
2012-09-09 02:26:38 +0200

    Change graphics

--- src/se/liu/ida/geoza435/TDDC69/lab2/GraphicalViewer.java
+++ src/se/liu/ida/geoza435/TDDC69/lab2/GraphicalViewer.java
@@ -13,12 +13,12 @@ import java.awt.*;
  */
 public class GraphicalViewer extends JComponent {
     Board board;
-    final static int SQUARE_SIZE = 32;
+    final static int SQUARE_SIZE = 20;
     //EnumMap<SquareColor, Color> squareColors;
 
     public Color getSquareColor(SquareColor sColor) {
         if (sColor == null) {
-            return new Color(30, 30, 30);
+            return new Color(20, 20, 20);
         }
         switch (sColor) {
             case CYAN:
@@ -53,11 +53,14 @@ public class GraphicalViewer extends JComponent {
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
+        g2.fill(new Rectangle(getPreferredSize()));
+        drawBackground(g2, board);
         for (int i = 2; i < board.getHeight(); i++) {
             for (int j = 0; j < board.getWidth(); j++) {
-                paintBlock(g2,
-                        new Point(j * SQUARE_SIZE, (i - 2) * SQUARE_SIZE),
-                        getSquareColor(board.getSquare(j, i)));
+                if (board.getSquare(j, i) != null)
+                    paintBlock(g2,
+                            new Point(j * SQUARE_SIZE, (i - 2) * SQUARE_SIZE),
+                            getSquareColor(board.getSquare(j, i)));
             }
         }
         if (board.fallingBlock != null) {
@@ -71,10 +74,45 @@ public class GraphicalViewer extends JComponent {
         }
     }
 
+    private void drawBackground(Graphics2D g2, Board board) {
+
+
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.setPaint(new GradientPaint(
+                new Point(0, 0),
+                getSquareColor(null),
+                new Point(
+                        board.getWidth() * SQUARE_SIZE,
+                        0),
+                getSquareColor(null).darker()
+        ));
+
+        g2.fill(new Rectangle(getPreferredSize()));
+
+        for (int i = 0; i < board.getHeight() - 2; i++) {
+            for (int j = 0; j < board.getWidth(); j++) {
+                g2.setColor(getSquareColor(null).brighter());
+                g2.draw(new Rectangle(j * SQUARE_SIZE, i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE));
+            }
+        }
+    }
+
     private void paintBlock(Graphics2D g2, Point position, Color color) {
         g2.setColor(color);
-        g2.fill(new Rectangle(position.x, position.y, SQUARE_SIZE, SQUARE_SIZE));
-        g2.setColor(Color.BLACK);
-        g2.draw(new Rectangle(position.x, position.y, SQUARE_SIZE, SQUARE_SIZE));
+        if (!color.equals(getSquareColor(null))) {
+            g2.setPaint(new GradientPaint(
+                    new Point(position.x, position.y),
+                    color,
+                    new Point(position.x, SQUARE_SIZE + position.y),
+                    color.darker()
+
+            ));
+            g2.fill(new Rectangle(position.x, position.y, SQUARE_SIZE, SQUARE_SIZE));
+            g2.setColor(color.darker().darker());
+            g2.draw(new Rectangle(position.x, position.y, SQUARE_SIZE, SQUARE_SIZE));
+        }
+
     }
 }
